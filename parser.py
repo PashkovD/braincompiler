bfc090551d01404144f39b9ad867c05678c7b73e
PashkovD <dimon2006763@gmail.com>
2022-09-24 15:38:04 +0500

    Optimizing parser

--- parser.py
+++ parser.py
@@ -23,20 +23,10 @@ class CodeParser:
 
     start = "file"
 
-    def p_error(self, p):
-        pass
-
     def p_file(self, p):
         """file :
                 | file declaration
-                | file assembler
-                | file astgoto
-                | file astout
-                | file astin
-                | file astset
-                | file astwhile
-                | file astif
-                | file astif_elif"""
+                | file code"""
         if len(p) == 1:
             p[0] = ASTFile()
             p[0].declarations = self.declarations
@@ -50,21 +40,18 @@ class CodeParser:
                 return
             p[0].code.append(p[2])
 
-    def p_code_block_start(self, p):
-        """code_block   : '{' """
+    def p_block_code_s_start(self, p):
+        """block_code_s : '{' """
         p[0] = []
 
-    def p_code_block_code(self, p):
-        """code_block   : code_block assembler
-                        | code_block astgoto
-                        | code_block astout
-                        | code_block astin
-                        | code_block astset
-                        | code_block astwhile
-                        | code_block astif
-                        | code_block astif_elif"""
+    def p_block_code_s_code(self, p):
+        """block_code_s : block_code_s code"""
         p[0] = p[1]
         p[0].append(p[2])
+
+    def p_block_code(self, p):
+        """block_code   : block_code_s '}'"""
+        p[0] = p[1]
 
     def p_declaration(self, p):
         """declaration  : int ID '=' INTEGER ';'
@@ -77,26 +64,26 @@ class CodeParser:
             p[4] = ord(p[4])
         p[0] = ASTDeclaration(p[2], p[4])
 
-    def p_assembler(self, p):
-        """assembler  : asm '(' STRING ')' ';'"""
+    def p_code_asm(self, p):
+        """code : asm '(' STRING ')' ';'"""
         if not all(i in "+-[]<>.," for i in p[3]):
             raise Exception(f"[:{p.slice[1].lineno}]Incorrect symbol in asm line")
         p[0] = ASTAssembler(p[3])
 
-    def p_astgoto(self, p):
-        """astgoto  : goto ID ';'"""
+    def p_code_goto(self, p):
+        """code  : goto ID ';'"""
         if p[2] not in self.declarations.keys():
             raise Exception(f"[:{p.slice[1].lineno}]Unknown ID {p[2]}")
         p[0] = ASTGoto(p[2])
 
-    def p_astin(self, p):
-        """astin  : in ID ';'"""
+    def p_code_in(self, p):
+        """code  : in ID ';'"""
         if p[2] not in self.declarations.keys():
             raise Exception(f"[:{p.slice[1].lineno}]Unknown ID {p[2]}")
         p[0] = ASTIn(p[2])
 
-    def p_astout(self, p):
-        """astout  : out ID ';'"""
+    def p_code_out(self, p):
+        """code  : out ID ';'"""
         if p[2] not in self.declarations.keys():
             raise Exception(f"[:{p.slice[1].lineno}]Unknown ID {p[2]}")
         p[0] = ASTOut(p[2])
@@ -114,8 +101,8 @@ class CodeParser:
         p[0] = p[1]
         p[0].append(p[3])
 
-    def p_astset_int(self, p):
-        """astset   : left_operands '=' INTEGER ';'
+    def p_code_set_int(self, p):
+        """code     : left_operands '=' INTEGER ';'
                     | left_operands IADD INTEGER ';'
                     | left_operands ISUB INTEGER ';'
                     | left_operands '=' STRING ';'
@@ -138,8 +125,8 @@ class CodeParser:
             case _ as e:
                 raise Exception(e)
 
-    def p_astset_var(self, p):
-        """astset   : left_operands '=' ID ';'
+    def p_code_set_var(self, p):
+        """code   : left_operands '=' ID ';'
                     | left_operands IADD ID ';'
                     | left_operands ISUB ID ';'"""
         decs = self.declarations.keys()
@@ -157,39 +144,45 @@ class CodeParser:
             case _ as e:
                 raise Exception(e)
 
-    def p_astwhile(self, p):
-        """astwhile : while '(' ID ')' code_block '}'"""
+    def p_code_while(self, p):
+        """code : while '(' ID ')' block_code"""
         if p[3] not in self.declarations.keys():
             raise Exception(f"[:{p.slice[1].lineno}]Unknown ID {p[3]}")
 
         p[0] = ASTWhile(p[3], p[5])
 
     def p_astif(self, p):
-        """astif : if '(' ID ')' code_block '}'"""
+        """astif : if '(' ID ')' block_code"""
         if p[3] not in self.declarations.keys():
             raise Exception(f"[:{p.slice[1].lineno}]Unknown ID {p[3]}")
 
         p[0] = ASTIf(p[3], p[5])
 
     def p_astif_elif_if(self, p):
-        """astif_elif : astif elif '(' ID ')' code_block '}'"""
+        """astif_elif : astif elif '(' ID ')' block_code"""
         if p[4] not in self.declarations.keys():
             raise Exception(f"[:{p.slice[1].lineno}]Unknown ID {p[4]}")
 
         p[0] = ASTIfElif([(p[1].test_var, p[1].code), (p[4], p[6])])
 
     def p_astif_elif_elif(self, p):
-        """astif_elif : astif_elif elif '(' ID ')' code_block '}'"""
+        """astif_elif : astif_elif elif '(' ID ')' block_code"""
         if p[4] not in self.declarations.keys():
             raise Exception(f"[:{p.slice[1].lineno}]Unknown ID {p[4]}")
         p[0] = p[1]
         p[0].data.append((p[4], p[6].code))
 
     def p_astif_else_if(self, p):
-        """astif_elif : astif else code_block '}'"""
+        """astif_else : astif else block_code"""
         p[0] = ASTIfElif([(p[1].test_var, p[1].code)], p[3])
 
     def p_astif_else_elif(self, p):
-        """astif_elif : astif_elif else code_block '}'"""
+        """astif_else : astif_elif else block_code"""
         p[0] = p[1]
         p[0].code_else = p[3]
+
+    def p_code_if(self, p):
+        """code : astif
+                | astif_elif
+                | astif_else"""
+        p[0] = p[1]
