feca720aa3408ea1c9843f531fdb53021737a198
PashkovD <dimon2006763@gmail.com>
2022-09-27 20:09:40 +0500

    Reformatted main.py

--- main.py
+++ main.py
@@ -9,21 +9,26 @@ from linker import CodeLinker
 from parser import CodeParser
 
 
+def compile_code(data: str) -> str:
+    lexer = CodeLexer()
+    parser = CodeParser(lexer.tokens, lexer.literals)
+
+    a: ASTFile = (parser.parse(input=data + "\n", lexer=lexer.lexer))
+
+    if not parser.parser.errorok:
+        raise Exception
+
+    return CodeLinker(a).process()
+
+
 def main(args: List[str]):
     arg_parser = ArgumentParser(description="Compile simple language in brainfuck")
     arg_parser.add_argument('file', type=str, help='Input file')
     arg_parser.add_argument('-o', type=str, default=None, help='output file')
     names = arg_parser.parse_args(args[1:])
 
-    lexer = CodeLexer()
-    parser = CodeParser(lexer.tokens, lexer.literals)
     with open(names.file) as f:
-        a: ASTFile = (parser.parse(input=f.read() + "\n", lexer=lexer.lexer))
-
-    if not parser.parser.errorok:
-        raise Exception
-
-    data = CodeLinker(a).process()
+        data: str = compile_code(f.read())
 
     output = names.o
     if output is None:
@@ -31,7 +36,7 @@ def main(args: List[str]):
 
     with open(output, "w") as f:
         for i in range(0, len(data), 100):
-            f.write(data[i:i+100] + "\n")
+            f.write(data[i:i + 100] + "\n")
 
 
 if __name__ == '__main__':
