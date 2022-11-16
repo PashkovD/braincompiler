802baee9602fccba343656089c10396f663dc287
PashkovD <dimon2006763@gmail.com>
2022-11-16 11:38:37 +0500

    New tests

--- tests/test.py
+++ tests/test.py
@@ -20,7 +20,7 @@ class Tests(TestCase):
         out = tests.intepreter.process(compile_code(code), inp)
         self.assertTrue(all(map(eq, out, reversed(inp))))
 
-    def test_add_const(self):
+    def test_add_int(self):
         inp = b""
         left1 = random.randint(0, 255)
         right1 = random.randint(0, 255)
@@ -37,3 +37,99 @@ class Tests(TestCase):
         out = tests.intepreter.process(compile_code(code), inp)
         self.assertEqual((left1 + right1) % 256, out[0])
         self.assertEqual((left2 + right2) % 256, out[1])
+
+    def test_sub_int(self):
+        inp = b""
+        left1 = random.randint(0, 255)
+        right1 = random.randint(0, 255)
+        left2 = random.randint(0, 255)
+        right2 = random.randint(0, 255)
+        code = f"""
+            int a = {left1};
+            int b = {left2};
+            a -= {right1};
+            b -= {right2};
+            out a;
+            out b;
+                """
+        out = tests.intepreter.process(compile_code(code), inp)
+        self.assertEqual((left1 - right1 + 256) % 256, out[0])
+        self.assertEqual((left2 - right2 + 256) % 256, out[1])
+
+    def test_set_int(self):
+        inp = b""
+        left1 = random.randint(0, 255)
+        right1 = random.randint(0, 255)
+        left2 = random.randint(0, 255)
+        right2 = random.randint(0, 255)
+        code = f"""
+            int a = {left1};
+            int b = {left2};
+            a = {right1};
+            b = {right2};
+            out a;
+            out b;
+                """
+        out = tests.intepreter.process(compile_code(code), inp)
+        self.assertEqual(right1, out[0])
+        self.assertEqual(right2, out[1])
+
+    def test_add_var(self):
+        inp = b""
+        left1 = random.randint(0, 255)
+        right1 = random.randint(0, 255)
+        left2 = random.randint(0, 255)
+        right2 = random.randint(0, 255)
+        code = f"""
+                    int a = {left1};
+                    int b = {left2};
+                    int c = {right1};
+                    int d = {right2};
+                    a += c;
+                    b += d;
+                    out a;
+                    out b;
+                        """
+        out = tests.intepreter.process(compile_code(code), inp)
+        self.assertEqual((left1 + right1) % 256, out[0])
+        self.assertEqual((left2 + right2) % 256, out[1])
+
+    def test_sub_var(self):
+        inp = b""
+        left1 = random.randint(0, 255)
+        right1 = random.randint(0, 255)
+        left2 = random.randint(0, 255)
+        right2 = random.randint(0, 255)
+        code = f"""
+                    int a = {left1};
+                    int b = {left2};
+                    int c = {right1};
+                    int d = {right2};
+                    a -= c;
+                    b -= d;
+                    out a;
+                    out b;
+                        """
+        out = tests.intepreter.process(compile_code(code), inp)
+        self.assertEqual((left1 - right1 + 256) % 256, out[0])
+        self.assertEqual((left2 - right2 + 256) % 256, out[1])
+
+    def test_set_var(self):
+        inp = b""
+        left1 = random.randint(0, 255)
+        right1 = random.randint(0, 255)
+        left2 = random.randint(0, 255)
+        right2 = random.randint(0, 255)
+        code = f"""
+                    int a = {left1};
+                    int b = {left2};
+                    int c = {right1};
+                    int d = {right2};
+                    a = c;
+                    b = d;
+                    out a;
+                    out b;
+                        """
+        out = tests.intepreter.process(compile_code(code), inp)
+        self.assertEqual(right1, out[0])
+        self.assertEqual(right2, out[1])
